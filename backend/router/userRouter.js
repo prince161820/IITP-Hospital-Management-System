678b5495356fcdbff5216b6becc4fc9f5cb16d9c
Prince Kumar Singh <149302338+prince161820@users.noreply.github.com>
2025-01-25 14:10:47 +0530

    Update userRouter.js

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -1,53 +1,14 @@
-import express from "express";
-import {
-  addNewAdmin,
-  login,
-  patientRegister,
-  getAllDoctors,
-  getUserDetails,
-  logoutAdmin,
-  logoutPatient,
-  addNewDoctor
-} from "../controller/userController.js";
-
-import {
-  isAdminAuthenticated,
-  isPatientAuthenticated,
-  
-} from "../middlewares/auth.js";
-
-const router = express.Router();
- 
-router.post("/patient/register", patientRegister);
-router.post("/login", login);
-router.post("/admin/addnew", isAdminAuthenticated, addNewAdmin);
-router.get("/doctors", getAllDoctors);
-router.get("/admin/me", isAdminAuthenticated, getUserDetails);
-router.get("/patient/me", isPatientAuthenticated, getUserDetails);
-router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
-router.get("/patient/logout", isPatientAuthenticated, logoutPatient);
-router.post("/doctor/addnew", isAdminAuthenticated, addNewDoctor);
-
-
-
-  /*
-import {
-  addNewAdmin,
-  addNewDoctor,
-  getAllDoctors,
-  getUserDetails,
-  login,
-  logoutAdmin,
-  logoutPatient,
-  patientRegister,
-} from "../controller/userController.js";  
-import {
-  isAdminAuthenticated,
-  isPatientAuthenticated,
-} from "../middlewares/auth.js";
-
-const router = express.Router();
-
+import express from "express"
+import {patientRegister } from "../controller/userController.js";
+import { login } from "../controller/userController.js";
+import { addNewAdmin } from "../controller/userController.js";
+import { isAdminAuthenticated,isPatientAuthenticated } from "../middlewares/auth.js";
+import { getAllDoctors } from "../controller/userController.js";
+import { getUserDetails } from "../controller/userController.js";
+import { logoutAdmin } from "../controller/userController.js";
+import { logoutPatient} from "../controller/userController.js";
+import { addNewDoctor } from "../controller/userController.js";
+const router= express.Router();
 router.post("/patient/register", patientRegister);
 router.post("/login", login);
 router.post("/admin/addnew", isAdminAuthenticated, addNewAdmin);
@@ -57,7 +18,4 @@ router.get("/patient/me", isPatientAuthenticated, getUserDetails);
 router.get("/admin/me", isAdminAuthenticated, getUserDetails);
 router.get("/patient/logout", isPatientAuthenticated, logoutPatient);
 router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
-   */
-
-export default router; 
-  
+export default router;
